01c5379a2e660b9a2a1b69b1972175b386bfe5aa
Saamarth Gupta <saamarthgupta@gmail.com>
2017-10-18 01:38:33 +0530

    Added Ability to Read Temperature from CSV Files
    The code in comments is used to Transfer Maximum Temperature Info from a poorly formatted CSV File.
     Bugs have caused improper training of the Model.

--- LinearRegression.cpp
+++ LinearRegression.cpp
@@ -4,28 +4,27 @@
 
 using namespace std;
 
-LinearRegression::LinearRegression(double x[], double y[], int m) {
+LinearRegression::LinearRegression(double x[], double y[], int m) 
+{
     this->x = x;
     this->y = y;
     this->m = m;
 }
 
-void LinearRegression::train(double alpha, int iterations) {
+void LinearRegression::train(double alpha, int iterations) 
+{
     double *J = new double[iterations];
     this->theta = gradient_descent(x, y, alpha, iterations, J, m);
-
-    cout << "J = ";
-    for (int i = 0; i < iterations; ++i) {
-        cout << J[i] << ' ';
-    }
     cout << endl << "Theta: " << theta[0] << " " << theta[1] << endl;
 }
 
-double LinearRegression::predict(double x) {
+double LinearRegression::predict(double x) 
+{
     return h(x, theta);
 }
 
-double LinearRegression::compute_cost(double x[], double y[], double theta[], int m) {
+double LinearRegression::compute_cost(double x[], double y[], double theta[], int m) 
+{
     double *predictions = calculate_predictions(x, theta, m);
     double *diff = Utils::array_diff(predictions, y, m);
     double *sq_errors = Utils::array_pow(diff, m, 2);
@@ -37,7 +36,8 @@ double LinearRegression::h(double x, double theta[])
     return theta[0] + theta[1] * x;
 }
 
-double *LinearRegression::calculate_predictions(double x[], double theta[], int m) {
+double *LinearRegression::calculate_predictions(double x[], double theta[], int m) 
+{
     double *predictions = new double[m];
 
     // calculate h for each training example
@@ -48,7 +48,8 @@ double *LinearRegression::calculate_predictions(double x[], double theta[], int 
     return predictions;
 }
 
-double *LinearRegression::gradient_descent(double x[], double y[], double alpha, int iters, double *J, int m) {
+double *LinearRegression::gradient_descent(double x[], double y[], double alpha, int iters, double *J, int m) 
+{
     double *theta = new double[2];
     theta[0] = 1;
     theta[1] = 1;
--- main.cpp
+++ main.cpp
@@ -1,38 +1,144 @@
 #include <iostream>
+#include <fstream>
 #include "LinearRegression.h"
 
 using namespace std;
 
+/*class weatherdata
+{
+public:
+	double days[1000];
+	double temperatures[1000];
+	int count;
+	weatherdata()
+	{
+		count=0;
+	}
+	void readweatherdata(int choice);
+};
+
+void weatherdata::readweatherdata(int choice)
+{
+	fstream file;
+    string filename;
+	/*if(choice==1)
+        filename="../Database/INDELHI.csv";
+	else if(choice == 2)
+		filename = "../Database/INMUMBAI.csv";
+    else if(choice == 3)
+		filename = "../Database/INKOLKATA.csv";
+	else
+		filename = "../Database/INCHENNAI.csv";
+    file.open("../Database/INDELHI.csv", fstream::in);
+
+	if(!file)
+		cout<<"File Not Opened.\n";
+	else
+	{
+		int flag;
+	    string date;
+	    int i=0;
+	    int countt,loc=0;
+
+	    while(file.good())
+	    {
+	        countt=0;
+	    	getline ( file, date );
+
+	        int sze=date.size();
+	        for(i=0;i<sze;i++)
+	        {
+	            if(date[i]=='\t')
+	                countt++;
+	            if(countt==3)
+	            {
+	                countt=0;
+	                break;
+	            }
+	        }
+	        i++;
+	        int j;
+	        flag=0;
+			if(count==50)
+				break;
+	        for(j=i;j<sze;j++)
+	        {
+	            if(date[j]=='.')
+	            {
+	                temperatures[loc]=10*((int)date[i]-'0')+((int)date[i+1]-'0')+((double)date[j+1]-'0')/10;
+	                flag=1;
+	                break;
+	            }
+	        }
+	        if(flag==0)
+	            temperatures[loc]=10*((int)date[i]-'0')+(int)date[i+1]-'0';
+	        int ran=loc;
+	        days[ran]=++loc;
+			count++;
+
+	    }
+	}
+}
+*/
 void days_vs_temp()
 {
-    double days[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    double temperatures[] = {29, 22, 33, 34, 29, 35, 40, 32, 29, 33, 30, 43, 39, 34, 36};
+	/*weatherdata l1;*/
+	double days[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+	double temperatures[] = {29, 22, 33, 34, 29, 35, 40, 32, 29, 33, 30, 43, 39, 34, 36,27.97};
+	/*l1.readweatherdata(choice);*/
+    LinearRegression weatherpred(days,temperatures, 16);
 
-    LinearRegression weatherpred(days,temperatures, 15);
 
     cout << "Enter learning rate alpha (default: 0.01): ";
     double alpha;
     cin>>alpha;
 
-    cout << "Enter number of iterations (default: 1500): ";
+    cout << "Enter number of iterations (default: 2000): ";
     int iterations;
     cin >> iterations;
 
     cout << "Training model..." << endl;
     weatherpred.train(alpha, iterations);
-
-    cout << "Model has been trained, enter Day :  ";
     double x;
-    cin >> x;
+  	int day=3;
+	
+    for(x=1;x<=20;x++)
+    {
+	    double temperature = weatherpred.predict(x);
 
-    double temperature = weatherpred.predict(x);
-    cout << "Estimated temperature: " << temperature << " C" << endl;
+	    cout << "Estimated temperature for "<<++day<<"/08/2017 is : " << temperature << " C" << endl;	//Temperature is Read in F
+	}
 }
 
 
 int main()
 {
-        days_vs_temp();
+
+    int choice;
+    check:											//Goto label JUMP
+	cout<<"Welcome to Weather Prediction Wizard.\n";
+	
+	cout<<"1. Delhi\n";
+	cout<<"2. Chennai\n";
+	cout<<"3. Mumbai\n";
+	cout<<"4. Kolkata\n";
+	cout<<"Select The City : \n";
+		cin>>choice;
+	if(choice==1)
+		cout<<"You Have Chosen Delhi.\n";
+	else if(choice==2)
+		cout<<"You Have Chosen Chennai.\n";
+	else if(choice==3)
+		cout<<"You Have Chosen Mumbai.\n";
+	else if(choice==4)
+		cout<<"You Have Chosen Kolkata.\n";
+	else
+	{
+		cout<<"You Have Chosen the Wrong choice. Try Again.\n";
+		goto check;
+	}
+
+    days_vs_temp();
 
         cout << endl << endl;
 
